235ebc647bd6ebcf89c94247f3c8993320388351
1233-Naveen <naveen.sunkara@motivitylabs.com>
2023-10-25 16:12:36 +0530

    App Icon and Splash Screen

--- App.jsx
+++ App.jsx
@@ -1,7 +1,16 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React,{useEffect} from 'react'
+import SplashScreen from 'react-native-splash-screen'
+import { Platform } from 'react-native'
 
 const App = () => {
+ useEffect(()=>{
+  if(Platform.OS==='android'){
+    SplashScreen.hide()
+  }
+  
+ },[])
+
   return (
     <View>
       <Text>Food Delivery App</Text>
