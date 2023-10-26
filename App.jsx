6bbdf2ce85ee37e098f799e2aceff9d600bf044b
1233-Naveen <naveen.sunkara@motivitylabs.com>
2023-10-26 14:25:19 +0530

    reverted errors and back to previous

--- App.jsx
+++ App.jsx
@@ -1,20 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React,{useEffect} from 'react'
+import React, { useEffect } from 'react'
+import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
 import { Platform } from 'react-native'
+import {Carousel,Pagination} from 'react-native-snap-carousel'
+import {splashScreenData} from './src/Utility/statData'
+import { Dimensions } from 'react-native'
+
+
 
 const App = () => {
- useEffect(()=>{
-  if(Platform.OS==='android'){
-    SplashScreen.hide()
-  }
+
+  const {width,height} = Dimensions.get('window')
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      SplashScreen.hide()
+      console.log(splashScreenData)
+    }
+
+  }, [])
   
- },[])
+
 
   return (
-    <View>
-      <Text>Food Delivery App</Text>
-    </View>
+    <SafeAreaView>
+      <Text style={{flexGrow:2}}>Hello kkks</Text>
+    </SafeAreaView>
   )
 }
 
---
+++ src/Utility/statData.js
@@ -0,0 +1,19 @@
+
+export const splashScreenData = [{
+    id:1,
+    title:"screen 1",
+    imageUrl:require('../assets/splash_screen_slide_1.png')
+    
+
+},
+{
+    id:2,
+    title:"screen 2",
+    imageUrl:require('../assets/splash_screen_slide_2.png')
+
+},{
+    id:3,
+    title:"screen 3",
+    imageUrl:require('../assets/splash_screen_slide_3.png')
+
+}]
