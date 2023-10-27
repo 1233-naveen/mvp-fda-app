91d47c1f083fd06761e2dc1ff08660c65a91ee54
1233-Naveen <naveen.sunkara@motivitylabs.com>
2023-10-27 06:39:08 +0530

    Naviagtion and splash screen working

--- App.jsx
+++ App.jsx
@@ -1,19 +1,46 @@
 
 import React, { useEffect } from 'react'
-import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
+import { StyleSheet, Text, View, SafeAreaView, Image, Pressable,Button } from 'react-native'
 import SplashScreen from 'react-native-splash-screen'
 import { Platform } from 'react-native'
-import {Carousel,Pagination} from 'react-native-snap-carousel'
-import {splashScreenData} from './src/Utility/statData'
+import { Carousel, Pagination } from 'react-native-snap-carousel'
+import { splashScreenData } from './src/Utility/statData'
 import { Dimensions } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+
+const Stack = createNativeStackNavigator()
 
 
 
+function HomeScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate('Login')}
+      />
+    </View>
+  );
+}
 
-const Stack = createNativeStackNavigator();
+function LoginScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Login Screen</Text>
+      <Button
+        title="Home"
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+}
+
 const App = () => {
 
-  const {width,height} = Dimensions.get('window')
+  const { width, height } = Dimensions.get('window')
 
   useEffect(() => {
     if (Platform.OS === 'android') {
@@ -22,14 +49,16 @@ const App = () => {
     }
 
   }, [])
-  
 
 
   return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
 
-    <SafeAreaView>
-      <Text style={{flexGrow:2}}>Hello kkks</Text>
-    </SafeAreaView>
   )
 }
 
--- android/app/src/main/java/com/fooddeliveryapp/MainActivity.java
+++ android/app/src/main/java/com/fooddeliveryapp/MainActivity.java
@@ -10,6 +10,12 @@ import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
+// @Override
+//   protected void onCreate(Bundle savedInstanceState) {
+//     super.onCreate(null);
+//   }
+
+
 
 @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +31,7 @@ public class MainActivity extends ReactActivity {
     return "FoodDeliveryApp";
   }
 
- @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
+ 
   /**
    * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
    * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
